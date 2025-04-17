a1c2d70c5eac8e1383b880d12ef7acdd42bf21ed
Michaela Krakowka <michaela.krakowka@gmail.com>
2025-04-17 17:28:51 +0200

    change MainStory

--- src/components/MainStory.js
+++ src/components/MainStory.js
@@ -10,8 +10,7 @@ export const MainStory = ({ isCLicked }) => {
             Můj dědeček zemřel náhle. Lékaři to označili za srdeční selhání, ale
             žádná pitva neproběhla. Když jsme ho našli, seděl u stolu ve své
             pracovně, oči dokořán, ústa otevřená jako by naposledy křičel – ale
-            nikdo ho neslyšel. Byl úplně sám. <br />
-            Nebo aspoň tak to tvrdí ostatní.
+            nikdo ho neslyšel. Byl úplně sám. Nebo aspoň tak to tvrdí ostatní.
             <br />
             Po pohřbu mi přišel balíček. Bez zpáteční adresy. Uvnitř byla jen
             zažloutlá obálka, na ní moje jméno psané jeho písmem. A v ní… jeden
