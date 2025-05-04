0eadefb791244eb3c89d2106210c152b5b038ff8
Michaela Krakowka <michaela.krakowka@gmail.com>
2025-05-04 13:57:05 +0200

    add ReactGA

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
+import ReactGA from "react-ga4";
 import { Header } from "./components/Header";
 import { GameBook } from "./components/GameBook";
 
+const TRACKING_ID = "G-S4F2CQVK9Z";
+
 export const App = () => {
+  React.useEffect(() => {
+    ReactGA.initialize(TRACKING_ID);
+    ReactGA.send("pageview");
+  }, []);
+
   const [isStoryStarted, setIsStoryStarted] = React.useState(false);
 
   const startStory = () => {
