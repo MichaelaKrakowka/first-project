ee68b2a639b3d86d2fe9a8b8ebe30eb2bfb67aa3
Michaela Krakowka <michaela.krakowka@gmail.com>
2025-06-01 03:13:38 +0200

    add fight component

--- src/components/GameBook.js
+++ src/components/GameBook.js
@@ -2,7 +2,7 @@ import React from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { story } from "./story";
 import "./GameBook.css";
-import { Dice } from "./Dice";
+import { Fight } from "./Fight";
 
 export const GameBook = ({
   userName,
@@ -14,16 +14,6 @@ export const GameBook = ({
 }) => {
   const storyTopRef = React.useRef(null);
   const [showMinusStrength, setShowMinusStrength] = React.useState(false);
-
-  const [player, setPlayer] = React.useState({ dice1: null, dice2: null });
-  const [enemy, setEnemy] = React.useState({ dice1: null, dice2: null });
-
-  const playerTotal =
-    (diceRoll ? diceRoll : 0) +
-    (player.dice1 ? player.dice1 : 0) +
-    (player.dice2 ? player.dice2 : 0);
-  const enemyTotal =
-    (enemy.dice1 ? enemy.dice1 : 0) + (enemy.dice2 ? enemy.dice2 : 0);
   const currentPart = story[currentPartId];
 
   const goToNextPart = (id) => {
@@ -45,94 +35,18 @@ export const GameBook = ({
     }, 100);
   };
 
-  const playerFought = player.dice1 !== null && player.dice2 !== null;
-  const enemyFought = enemy.dice1 !== null && enemy.dice2 !== null;
-
   if (currentPartId === "fight") {
     return (
       <div className="story_text" ref={storyTopRef}>
         {currentPart.text}
-        <div className="fight_container">
-          <div className="fight_note">
-            <p>
-              Chceš zvednout kladivo, zničit přístroj a tím porazit nepřítele?
-            </p>
-            <p>
-              Součet hodu dvěma kostkami a tvé zbývající síly musí být alespoň
-              tak vysoký jako soupeřův.
-            </p>
-          </div>
-          <div className="dices">
-            <div className="fight_dice">
-              {userName}:
-              <Dice
-                diceRoll={player.dice1}
-                setDiceRoll={(newValue) =>
-                  setPlayer((prev) => {
-                    return {
-                      ...prev,
-                      dice1: newValue,
-                    };
-                  })
-                }
-              />{" "}
-              +{" "}
-              <Dice
-                diceRoll={player.dice2}
-                setDiceRoll={(newValue) =>
-                  setPlayer((prev) => {
-                    return {
-                      ...prev,
-                      dice2: newValue,
-                    };
-                  })
-                }
-              />
-              = {playerTotal}
-            </div>
-            <div className="fight_dice">
-              Nepřítel:
-              <Dice
-                diceRoll={enemy.dice1}
-                setDiceRoll={(newValue) =>
-                  setEnemy((prev) => {
-                    return { ...prev, dice1: newValue };
-                  })
-                }
-              />
-              +
-              <Dice
-                diceRoll={enemy.dice2}
-                setDiceRoll={(newValue) =>
-                  setEnemy((prev) => {
-                    return { ...prev, dice2: newValue };
-                  })
-                }
-              />
-              = {enemyTotal}
-            </div>
-          </div>
-
-          {playerFought && enemyFought && diceRoll !== null && (
-            <div className="finish_fight">
-              {playerTotal > enemyTotal &&
-                "Sebral jsem poslední zbytky síly a kladivem jsem trefil přímo doprostřed přístroje. Cítil jsem, jakoby se dům nadechl, jakoby čekal na tento okamžik. Dědeček otevřel dveře do něčeho, co neměl. A zavřel je, jak nejlépe dovedl. Já jsem je jen zapečetil. Aby už nikdo další neskončil v tom tichu."}
-              {playerTotal < enemyTotal &&
-                "Všechno se pomalu nořilo do temnoty hlubší než je smrt. Pak jsem najednou stál stranou. Viděl jsem, jak moje tělo klidně odchází z místnosti. Pak se na okamžik otočilo… a usmálo se. V tu chvíli jsem pochopil: já tu zůstal v tom tichu, ve tmě a někdo jiný právě začal žít můj život."}
-              {playerTotal === enemyTotal &&
-                "Sebral jsem poslední zbytky síly a kladivem jsem trefil přímo doprostřed přístroje. Cítil jsem, jakoby se dům nadechl, jakoby čekal na tento okamžik. Dědeček otevřel dveře do něčeho, co neměl. A zavřel je, jak nejlépe dovedl. Já jsem je jen zapečetil. Aby už nikdo další neskončil v tom tichu."}
-              <button
-                className="btn finish_btn"
-                onClick={() => goToNextPart("finish")}>
-                KONEC
-              </button>
-            </div>
-          )}
-        </div>
+        <Fight
+          userName={userName}
+          diceRoll={diceRoll}
+          goToNextPart={goToNextPart}
+        />
       </div>
     );
   }
-
   return (
     <div className="story_text" ref={storyTopRef}>
       {currentPart.text}
